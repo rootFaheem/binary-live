063f8e0ad4d3ca29de3a89813534f1cf20dc634d
rootFaheem <faheemtech1@gmail.com>
2019-10-25 22:21:41 +0530

    server prepared via express for React apps

--- server/server.js
+++ server/server.js
@@ -1,48 +1,17 @@
 const express = require("express");
 const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io").listen(server);
 
 let users = [];
 let connections = [];
 
-server.listen(process.env.PORT || 5000);
-console.log("server running at...", 5000);
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+const server = app.listen(5000, () => {
+  console.log("server running at...", 5000);
 });
 
-io.sockets.on("connection", socket => {
-  console.log("socket hit:");
+const io = require("socket.io")(server);
+
+io.on("connection", socket => {
+  console.log("new connection added to the list");
   connections.push(socket);
-  console.log("Connected: %s sockets connected", connections.length);
-
-  // Disconnect
-  socket.on("disconnect", data => {
-    // if (!socket.username) return;
-    users.splice(users.indexOf(socket.username), 1);
-    updateUsername();
-
-    connections.splice(connections.indexOf(socket));
-    console.log("Disconnected: %s sockets connection", connections.length);
-  });
-
-  // Send Messages
-  socket.on("send message", data => {
-    console.log("data:", data);
-    io.sockets.emit("new message", { msg: data, user: socket.username });
-  });
-
-  // New User
-  socket.on("new user", (data, callback) => {
-    callback(true);
-    socket.username = data;
-    users.push(socket.username);
-    updateUsername();
-  });
-
-  const updateUsername = () => {
-    io.sockets.emit("get users", users);
-  };
+  console.log("current list is:", connections.length);
 });
