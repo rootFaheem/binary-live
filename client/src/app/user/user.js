0640dcbb2fe59ae845ef6c1c8959cea97fd96ea7
rootFaheem <faheemtech1@gmail.com>
2019-10-26 23:20:26 +0530

    veritical listing -> connecting the name to chat

--- client/src/app/user/user.js
+++ client/src/app/user/user.js
@@ -1,24 +1,54 @@
 import React, { Component } from "react";
-import OpenSocket from "socket.io-client";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
-import { withStyles } from "@material-ui/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-import OnlineUserList from "./onlineUserList/onlineUserList";
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`
+  };
+}
 
 const styles = theme => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: "flex",
+    height: 224
   },
-  paper: {
-    padding: "20px",
-    textAlign: "center",
-    color: "#717171"
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`
   }
 });
 
-class User extends Component {
+class VerticalTabs extends Component {
   state = {
     users: [
       { name: "Faheem Ahmad", imgURL: "" },
@@ -40,27 +70,117 @@ class User extends Component {
       }
     ]
   };
-
-  componentDidMount = () => {
-    OpenSocket("/");
-  };
   render() {
-    const { classes } = this.props;
+    const classes = this.props;
+
     return (
       <div className={classes.root}>
-        <Grid container spacing={3}>
-          <Grid container item xs={12} spacing={6}>
-            <Grid item xs={4}>
-              <OnlineUserList users={this.state.users}></OnlineUserList>
-            </Grid>
-            <Grid item xs={8}>
-              <Paper className={classes.paper}>CHAT AREA</Paper>
-            </Grid>
-          </Grid>
-        </Grid>
+        <Tabs
+          orientation="vertical"
+          variant="scrollable"
+          value={"Item 1"}
+          onChange
+          aria-label="Vertical tabs example"
+          className={classes.tabs}
+        >
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+          <Tab label="Item Four" {...a11yProps(3)} />
+          <Tab label="Item Five" {...a11yProps(4)} />
+          <Tab label="Item Six" {...a11yProps(5)} />
+          <Tab label="Item Seven" {...a11yProps(6)} />
+        </Tabs>
+        <TabPanel value={"Item 1"} index={0}>
+          Item One
+        </TabPanel>
+        <TabPanel value={"Item 1"} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={"Item 1"} index={2}>
+          Item Three
+        </TabPanel>
+        <TabPanel value={"Item 1"} index={3}>
+          Item Four
+        </TabPanel>
+        <TabPanel value={"Item 1"} index={4}>
+          Item Five
+        </TabPanel>
+        <TabPanel value={"Item 1"} index={5}>
+          Item Six
+        </TabPanel>
+        <TabPanel value={"Item 1"} index={6}>
+          Item Seven
+        </TabPanel>
       </div>
     );
   }
 }
+export default withStyles(styles)(VerticalTabs);
 
-export default withStyles(styles)(User);
+// import React, { Component } from "react";
+// import OpenSocket from "socket.io-client";
+
+// import { withStyles } from "@material-ui/styles";
+// import Paper from "@material-ui/core/Paper";
+// import Grid from "@material-ui/core/Grid";
+
+// import OnlineUserList from "./onlineUserList/onlineUserList";
+
+// const styles = theme => ({
+//   root: {
+//     flexGrow: 1
+//   },
+//   paper: {
+//     padding: "20px",
+//     textAlign: "center",
+//     color: "#717171"
+//   }
+// });
+
+// class User extends Component {
+//   state = {
+//     users: [
+//       { name: "Faheem Ahmad", imgURL: "" },
+//       {
+//         name: "Elon Musk",
+//         imgURL: ""
+//       },
+//       {
+//         name: "Jeff Bezos",
+//         imgURL: ""
+//       },
+//       {
+//         name: "Peter Theil",
+//         imgURL: ""
+//       },
+//       {
+//         name: "Faisal Computer",
+//         imgURL: ""
+//       }
+//     ]
+//   };
+
+//   componentDidMount = () => {
+//     OpenSocket("/");
+//   };
+//   render() {
+//     const { classes } = this.props;
+//     return (
+//       <div className={classes.root}>
+//         <Grid container spacing={3}>
+//           <Grid container item xs={12} spacing={6}>
+//             <Grid item xs={4}>
+//               <OnlineUserList users={this.state.users}></OnlineUserList>
+//             </Grid>
+//             <Grid item xs={8}>
+//               <Paper className={classes.paper}>CHAT AREA</Paper>
+//             </Grid>
+//           </Grid>
+//         </Grid>
+//       </div>
+//     );
+//   }
+// }
+
+// export default withStyles(styles)(User);
