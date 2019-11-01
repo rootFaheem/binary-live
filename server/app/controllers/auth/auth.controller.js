1b8c98e3208328086b89aee0f1c557ef0edb5dc9
rootFaheem <faheemtech1@gmail.com>
2019-11-01 20:58:17 +0530

    mongo server issue

--- server/app/controllers/auth/auth.controller.js
+++ server/app/controllers/auth/auth.controller.js
@@ -28,8 +28,6 @@ const registerUser = async (req, res) => {
           message: "email is already registerd"
         });
       } else {
-
-
         if (pass1 !== pass2) {
           return res.status(303).json({
             success: "false",
@@ -37,45 +35,47 @@ const registerUser = async (req, res) => {
           });
         }
 
-        await bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(password, salt, (err, hash) => {
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(pass1, salt, (err, hash) => {
             if (err) throw err;
 
             hashPass = hash;
+
+            const newUserModel = new UserModel({
+              name,
+              email,
+              password: hashPass
+            });
+
+            console.log("newUserModel", newUserModel);
           });
         });
 
-        const newUserModel = new UserModel({
-          name,
-          email,
-          password: hashPass
-        });
-
-        await newUserModel.save();
+        // await newUserModel.save();
       }
     });
 
-    Joi.validate(
-      values,
-      {
-        abortEarly: true
-      },
-      err => {
-        if (err) {
-          console.log("err:", err);
-          return res.status(422).json({
-            success: false,
-            message: err.message
-          });
-        }
-        console.log("user.creating");
-        return res.status(200).json({
-          success: true,
-          message: "you are no registered",
-          value
-        });
-      }
-    );
+    // Joi.validate(
+    //   values,
+    //   {
+    //     abortEarly: true
+    //   },
+    //   err => {
+    //     if (err) {
+    //       console.log("err:", err);
+    //       return res.status(422).json({
+    //         success: false,
+    //         message: err.message
+    //       });
+    //     }
+    //     console.log("user.creating");
+    //     return res.status(200).json({
+    //       success: true,
+    //       message: "you are no registered",
+    //       value
+    //     });
+    //   }
+    // );
   } catch (err) {
     res.status(500).json({
       success: false,
