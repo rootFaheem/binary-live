dc54ebe69b5225d48502ae1ddcfc6c503b93fcdc
rootFaheem <faheemtech1@gmail.com>
2019-11-03 23:12:57 +0530

    login form

---
+++ client/src/app/forms/login.form.js
@@ -0,0 +1,142 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+import { loginUserAction } from "../../../store/actions/auth.action";
+
+const styles = theme => ({
+  "@global": {
+    body: {
+      backgroundColor: theme.palette.common.white
+    }
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+});
+
+class SignIn extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  emailChangedhandler = e => {
+    this.setState({
+      email: e.target.value
+    });
+  };
+
+  passwordChangedhandler = e => {
+    this.setState({
+      password: e.target.value
+    });
+  };
+
+  signInHandler = e => {
+    e.preventDefault();
+
+    const { email, password } = this.state;
+    const data = {
+      email,
+      password,
+      type: "login"
+    };
+
+    this.props.loginUserAction(data);
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form
+            className={classes.form}
+            noValidate
+            onSubmit={this.signInHandler}
+          >
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={this.emailChangedhandler}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={this.passwordChangedhandler}
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+          </form>
+        </div>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = ({ loginReducer }) => {
+  return {
+    _auth: loginReducer
+  };
+};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { loginUserAction }
+  )(withStyles(styles)(SignIn))
+);
