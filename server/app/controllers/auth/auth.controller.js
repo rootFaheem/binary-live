84e7104f38244fb7e5ed7dac2318758698b1d995
rootFaheem <faheemtech1@gmail.com>
2019-11-03 21:14:08 +0530

    bcrypt hash => working

--- server/app/controllers/auth/auth.controller.js
+++ server/app/controllers/auth/auth.controller.js
@@ -6,8 +6,6 @@ const bcrypt = require("bcryptjs");
 const { registerUserSchema } = require("./auth.validation");
 
 const registerUser = async (req, res) => {
-  console.log("req.body", req.body);
-
   const { name, email, pass1, pass2 } = req.body;
   let hashPass = "";
 
@@ -17,8 +15,6 @@ const registerUser = async (req, res) => {
     pass1,
     pass2
   };
-
-  console.log("value:", values);
 
   try {
     UserModel.findOne({ email }).then(user => {
@@ -36,7 +32,7 @@ const registerUser = async (req, res) => {
         }
 
         bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(pass1, salt, (err, hash) => {
+          bcrypt.hash(pass1, salt, async (err, hash) => {
             if (err) throw err;
 
             hashPass = hash;
@@ -47,35 +43,17 @@ const registerUser = async (req, res) => {
               password: hashPass
             });
 
-            console.log("newUserModel", newUserModel);
+            await newUserModel.save();
+
+            return res.status(200).json({
+              success: true,
+              message: "user registered successfully"
+            });
           });
         });
-
-        // await newUserModel.save();
       }
+      return;
     });
-
-    // Joi.validate(
-    //   values,
-    //   {
-    //     abortEarly: true
-    //   },
-    //   err => {
-    //     if (err) {
-    //       console.log("err:", err);
-    //       return res.status(422).json({
-    //         success: false,
-    //         message: err.message
-    //       });
-    //     }
-    //     console.log("user.creating");
-    //     return res.status(200).json({
-    //       success: true,
-    //       message: "you are no registered",
-    //       value
-    //     });
-    //   }
-    // );
   } catch (err) {
     res.status(500).json({
       success: false,
