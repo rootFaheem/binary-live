519ab026aed0e2ef615242c1c51923a6a288926f
rootFaheem <faheemtech1@gmail.com>
2019-11-08 11:53:03 +0530

    error handling with status code

--- server/app/graphql/resolvers.js
+++ server/app/graphql/resolvers.js
@@ -13,7 +13,7 @@ module.exports = {
     const errors = [];
     if (!validator.isEmail(userInput.email)) {
       errors.push({
-        errMsg: "email not valid"
+        errMsg: "email is not valid"
       });
     }
 
@@ -28,6 +28,8 @@ module.exports = {
 
     if (errors.length > 0) {
       const error = new Error("Invalid input.");
+      error.data = errors;
+      error.code = 422;
       throw error;
     }
 
--- server/server.js
+++ server/server.js
@@ -38,7 +38,18 @@ app.use(
   graphqlhttp({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
-    graphiql: true
+    graphiql: true,
+    formatError(err) {
+      if (!err.originalError) {
+        return err;
+      }
+
+      const data = err.originalError.data;
+      const message = err.message || "An error occured";
+      const code = err.originalError.code || 500;
+
+      return { message, status: code, data };
+    }
   })
 );
 
