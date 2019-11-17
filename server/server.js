2f597d46e7ab5ec8d20e1fb76d4414b5965592ed
rootFaheem <faheemtech1@gmail.com>
2019-11-17 19:46:48 +0530

    starting with apollo server

--- server/server.js
+++ server/server.js
@@ -1,51 +1,13 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const { ApolloServer, gql } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schema");
 
-const { PORT, MONGODB_URI } = require("./configs/keys");
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
 });
 
-mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("MongoDb conncected...");
-  })
-  .catch(err => {
-    console.log(err);
-  });
+server.applyMiddleware({ app });
 
-const server = app.listen(PORT, () => {
-  console.log("server running at...", PORT);
-});
-
-// const io = require("./app/utils/socket").init(server);
-
-// io.on("connection", socket => {
-//   console.log("new connection added to the list");
-//   connections.push(socket);
-//   console.log("current list is:", connections.length);
-// });
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
