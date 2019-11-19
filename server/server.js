f6f2bf5c62fe9bc77aa33c19e3a23814e8059d41
rootFaheem <faheemtech1@gmail.com>
2019-11-20 00:02:03 +0530

    mongo and express server order

--- server/server.js
+++ server/server.js
@@ -1,8 +1,5 @@
 const { ApolloServer, gql } = require("apollo-server-express");
 const express = require("express");
-
-const app = express();
-
 const mongoose = require("mongoose");
 
 const typeDefs = require("./app/typeDefs/typeDefs");
@@ -10,29 +7,35 @@ const resolvers = require("./app/resolvers/resolvers");
 
 const { MONGODB_URI, PORT } = require("./configs/keys");
 
-mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("MongoDb conncected...");
-  })
-  .catch(err => {
-    console.log(err);
+const startServer = async () => {
+  const app = express();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
   });
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
+  server.applyMiddleware({ app });
 
-server.applyMiddleware({ app });
+  await mongoose
+    .connect(MONGODB_URI, {
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then(() => {
+      console.log("MongoDb conncected...");
+    })
+    .catch(err => {
+      console.log(err);
+    });
 
-app.listen(PORT, () =>
-  console.log(
-    `🚀  server is running at http://localhost:4000${server.graphqlPath} `
-  )
-);
+  app.listen(PORT, () =>
+    console.log(
+      `🚀  server is running at http://localhost:4000${server.graphqlPath} `
+    )
+  );
+};
+
+startServer();
